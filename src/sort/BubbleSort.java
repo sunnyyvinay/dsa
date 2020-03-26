22634d32aa0d44c273b2f177550296bdcaed5198
Sunny <sunny@vsinglas-MacBook-Pro.local>
2020-03-25 17:08:39 -0700

    Added BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,41 @@
+package sort;
+
+/*
+ * Sample program for Bubble Sort algorithm
+ *
+ * https://www.geeksforgeeks.org/bubble-sort/
+ *
+ * Bubble Sort repeatedly swaps the adjacent elements if they are in wrong order. The algorithm stops when it goes through one pass
+ * without any swaps.
+ */
+
+public class BubbleSort {
+    void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    /* Prints the array */
+    void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+        // Driver method to test above
+    public static void main(String args[]) {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
