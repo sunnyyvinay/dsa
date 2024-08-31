0bb0438ee91e33ff8b31755fe69921b311a44680
Sunny Vinay <sunnyvinay7@gmail.com>
2024-08-30 17:39:30 -0700

    stack/queue structs

---
+++ src/c++/Queue.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 50;
+
+class Queue {
+public:
+    Queue() {
+        size = 0;
+        front = 0;
+        rear = -1;
+    }
+    bool isFull() const {
+        return size == MAX;
+    }
+    int top() {
+        return arr[front];
+    }
+    void enqueue(int val) {
+        if (!isFull()) {
+            rear = (rear + 1) % MAX;
+            arr[rear] = val;
+            size++;
+        }
+    }
+    int dequeue() {
+        if (size != 0) {
+            int val = arr[front];
+            front = (front + 1) % MAX;
+            size--;
+            return val;
+        }
+
+        return -1;
+    }
+private:
+    int arr[MAX]{};
+    int front;
+    int rear;
+    int size;
+};
+
+int main() {
+    Queue q;
+    q.enqueue(1);
+    cout << q.top() << endl; // 1
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.enqueue(6);
+    cout << q.dequeue() << endl; // 1
+    cout << q.dequeue() << endl; // 2
+
+    return 0;
+}
---
+++ src/c++/Stack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 50;
+
+class Stack {
+public:
+    Stack() {
+        top = 0;
+    }
+    bool isFull() const {
+        return top == MAX;
+    }
+    int peek() {
+        return arr[top-1];
+    }
+    void push(int val) {
+        if (!isFull()) {
+            arr[top] = val;
+            top++;
+        }
+    }
+    int pop() {
+        int val = arr[top-1];
+        top--;
+        return val;
+    }
+    int getSize() {
+        return top;
+    }
+private:
+    int arr[MAX]{};
+    int top;
+};
+
+int main() {
+    Stack stack;
+    stack.push(1);
+    cout << stack.peek() << endl; // 1
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.push(6);
+    cout << stack.pop() << endl; // 6
+    cout << stack.peek() << endl; // 5
+
+    return 0;
+}
