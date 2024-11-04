11e9df74d16d1df26be2801cce71be2660cf551d
Sunny Vinay <sunnyvinay7@gmail.com>
2024-11-03 18:02:10 -0800

    change dijkstras

--- src/c++/graph/Dijkstras.cpp
+++ src/c++/graph/Dijkstras.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-vector<int> findPath(vector<vector<int>>& graph, int startNode);
+class Edge {
+public:
+    int weight = 0;
+    int to = 0; // node
+
+    Edge(int to, int weight) {
+        this->weight = weight;
+        this->to = to;
+    }
+};
+typedef pair<int, int> Pair; // 1 - distance, 2 - node
+vector<int> findPath(vector<vector<Edge*>>& graph, int startNode);
 
 /*
  * Dijkstra's ALGORITHM
  *
  * Finds the shortest path with the minimum weighted sum.
  * It calculates the path for every vertex from a given starting point.
+ * This implementation uses a min-priority_queue based on the distance.
  *
- * Time complexity: O(V^2 + E)
+ * Time complexity: O((m+n)log(n))
  *
  */
 
-vector<int> findPath(vector<vector<int>>& graph, int startNode) {
-    vector<int> distances(graph.size());
-    vector<bool> visited(graph.size());
-    for (int v = 0; v < graph.size(); v++) {
-        distances[v] = INT_MAX;
-        visited[v] = false;
-    }
+vector<int> findPath(vector<vector<Edge*>>& graph, int startNode) {
+    vector<int> distances(graph.size(), INT_MAX);
+    priority_queue<Pair, vector<Pair>, greater<Pair>> P;
+
     distances[startNode] = 0;
-
-    for (int j = 1; j < visited.size(); j++) {
-        int u = 1;
-        int minDist = INT_MAX;
-        for (int i = 0; i < distances.size(); i++) {
-            if (!visited[i] && distances[i] < minDist) {
-                minDist = distances[i];
-                u = i;
+    P.push(make_pair(0, startNode));
+    while (!P.empty()) {
+        Pair curr = P.top();
+        int currNode = curr.second;
+        P.pop();
+        for (Edge* neighbor : graph[curr.second]) {
+            if (distances[currNode] + neighbor->weight < distances[neighbor->to]) {
+                distances[neighbor->to] = distances[currNode] + neighbor->weight;
+                P.push(make_pair(distances[neighbor->to], neighbor->to));
             }
-        }
-
-        visited[u] = true;
-        for (int v = 0; v < graph[u].size(); v++) {
-            distances[graph[u][v]] = min(distances[graph[u][v]], distances[u] + 1); // if weighted graph, replace 1 with weight(u, v)
         }
     }
 
@@ -43,15 +49,18 @@ vector<int> findPath(vector<vector<int>>& graph, int startNode) {
 }
 
 int main() {
-    vector<vector<int>> graph = {
-            {1, 2}, // Node 0 is connected to nodes 1 and 2
-            {0, 3}, // Node 1 is connected to nodes 0 and 3
-            {0, 4}, // Node 2 is connected to nodes 0 and 4
-            {1},    // Node 3 is connected to node 1
-            {2}     // Node 4 is connected to node 2
+    vector<vector<Edge*>> graph = {
+            {new Edge(1, 2), new Edge(2, 2)},                               // Node 0 is connected to nodes 1, 2
+            {new Edge(0, 2), new Edge(4, 5), new Edge(3, 2)},    // Node 1 is connected to nodes 0, 3, 4
+            {new Edge(0, 2), new Edge(4, 2)},                               // Node 2 is connected to nodes 0, 4
+            {new Edge(1, 2), new Edge(4, 2)},                               // Node 3 is connected to node 1, 4
+            {new Edge(1, 5), new Edge(3, 2), new Edge(2, 2)}     // Node 4 is connected to node 1, 2, 3
     };
 
-    findPath(graph, 0);
+    vector<int> d = findPath(graph, 0);
+    for (int i : d) {
+        cout << i << " ";
+    }
     cout << endl;
 
     return 0;
